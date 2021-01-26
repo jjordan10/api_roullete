988efe85ce8efd720147f55cd4b39593788bf3c2
jjordan10 <46966930+jjordan10@users.noreply.github.com>
2021-01-26 18:50:31 -0500

    roullette_controller class was created

---
+++ Controllers/roullete_controller.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace api_roullete.Controllers
+{
+    public class Student
+    {
+        public int Id {get; set;}
+        public int apuesta {get; set;}
+        public bool estado {get; set;}
+    }
+    [ApiController]
+    [Route("[controller]")]
+    public class Roullete_controller : ControllerBase
+    {
+
+    }
